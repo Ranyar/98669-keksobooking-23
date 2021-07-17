82fae8d044ca7f6789320e06abb85f7578d2764b
Ranyar <r4n74r@gmail.com>
2021-07-17 18:25:50 +0500

    Отрисуй меня полностью. Часть 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 import './data.js';
 import {getAdsMarkup} from './get-ads-markup.js';
 import {ADS_COUNT} from './util.js';
+import {disableForms, enableForms} from './form.js';
 
 getAdsMarkup(ADS_COUNT);
+
+window.addEventListener('DOMContentLoaded', () => {
+  disableForms();
+  //Реализуем проверку работы активации форм по клику в зону логотипа.
+  document.querySelector('.promo').addEventListener('click', enableForms);
+});
