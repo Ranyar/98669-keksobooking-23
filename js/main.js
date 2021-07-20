050215e33e8e2508deb300b37fd325841925edf7
Ranyar <r4n74r@gmail.com>
2021-07-20 19:43:22 +0500

    Правда или действие (часть 2)

--- js/main.js
+++ js/main.js
@@ -2,16 +2,11 @@ import './data.js';
 import {getAdsMarkup} from './get-ads-markup.js';
 import {ADS_COUNT} from './util.js';
 import {disablePage, enablePage} from './page-activity-switcher.js';
-import {validateInputs} from './validate-inputs.js';
-import {replaceSubmitHandler} from './forms.js';
+import './validate-inputs.js';
 
 getAdsMarkup(ADS_COUNT);
-replaceSubmitHandler('.ad-form');
 
 disablePage();
 //Реализуем проверку работы активации форм по клику в зону логотипа.
 document.querySelector('.promo').addEventListener('click', enablePage);
-
-validateInputs();
-
 
--- js/validate-inputs.js
+++ js/validate-inputs.js
@@ -1,81 +1,87 @@
-const validateInputs = () => {
-  const adForm = document.querySelector('.ad-form');
-  const title = adForm.querySelector('#title');
-  const timein = adForm.querySelector('#timein');
-  const timeout = adForm.querySelector('#timeout');
-  const type = adForm.querySelector('#type');
-  const price = adForm.querySelector('#price');
-  const minPrices = {
-    'bungalow': 0,
-    'flat': 1000,
-    'hotel': 3000,
-    'house': 5000,
-    'palace': 10000,
-  };
-
-  const roomNumber = adForm.querySelector('#room_number');
-  const capacity = adForm.querySelector('#capacity');
-  const capacityOptions = {
-    1: `
+const adForm = document.querySelector('.ad-form');
+const title = adForm.querySelector('#title');
+const timein = adForm.querySelector('#timein');
+const timeout = adForm.querySelector('#timeout');
+const type = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+const minPrices = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+const MAX_PRICE = 1000000;
+const capacityOptions = {
+  1: `
     <option value="1" selected>для 1 гостя</option>
     `,
-    2: `
+  2: `
     <option value="1" selected>для 1 гостя</option>
     <option value="2">для 2 гостей</option>
     `,
-    3: `
+  3: `
     <option value="1" selected>для 1 гостя</option>
     <option value="2">для 2 гостей</option>
     <option value="3">для 3 гостей</option>
     `,
-    100: `
+  100: `
     <option value="0" selected>не для гостей</option>
     `,
-  };
-  const validateLength = (field) => {
-    const length = field.value.length;
-    const minLength = +field.getAttribute('minlength');
-    if (field.validity.tooShort) {
-      field.setCustomValidity(`Необходимо еще ${minLength - length} символов`);
-    } else {
-      field.setCustomValidity('');
-    }
-
-    field.reportValidity();
-  };
-  const validateNumber = (field, min, max) => {
-    if (field.validity.rangeUnderflow) {
-      field.setCustomValidity(`Минимальное значение для этого поля: ${min}`);
-    } else if (field.validity.rangeOverflow) {
-      field.setCustomValidity(`Максимальное значение для этого поля: ${max}`);
-    } else {
-      field.setCustomValidity('');
-    }
-
-    field.reportValidity();
-  };
-  const synchronizeTimeSelects = (mainField, dependentField) => {
-    dependentField.value = mainField.value;
-  };
-
-  const synchronizeCapacitySelects = (mainField, dependentField) => {
-    dependentField.innerHTML = capacityOptions[mainField.value];
-  };
-
-  const mapPriceWithType = (mapObject) => {
-    price.setAttribute('min', mapObject[type.value]);
-    price.setAttribute('placeholder', mapObject[type.value]);
-  };
-
-  mapPriceWithType(minPrices);
-  synchronizeCapacitySelects(roomNumber, capacity);
-
-  title.addEventListener('input', () => validateLength(title));
-  timein.addEventListener('change', () => synchronizeTimeSelects(timein, timeout));
-  timeout.addEventListener('change', () => synchronizeTimeSelects(timeout, timein));
-  type.addEventListener('change', () => mapPriceWithType(minPrices));
-  price.addEventListener('input', () => validateNumber(price, minPrices[type.value], price.getAttribute('max')));
-  roomNumber.addEventListener('change', () => synchronizeCapacitySelects(roomNumber, capacity));
 };
 
-export {validateInputs};
+const titleInputHandler = (evt) => {
+  const length = evt.target.value.length;
+  const minLength = +evt.getAttribute('minlength');
+
+  if (evt.target.validity.tooShort) {
+    evt.target.setCustomValidity(`Необходимо еще ${minLength - length} символов`);
+  } else {
+    evt.target.setCustomValidity('');
+  }
+
+  evt.target.reportValidity();
+};
+
+title.addEventListener('input', titleInputHandler);
+
+
+const priceInputHandler = (evt) => {
+  const min = minPrices[type.value];
+  if (evt.target.validity.rangeUnderflow) {
+    evt.target.setCustomValidity(`Минимальное значение для этого поля: ${min}`);
+  } else if (evt.target.validity.rangeOverflow) {
+    evt.target.setCustomValidity(`Максимальное значение для этого поля: ${MAX_PRICE}`);
+  } else {
+    evt.target.setCustomValidity('');
+  }
+  evt.target.reportValidity();
+};
+
+price.addEventListener('input', priceInputHandler);
+
+const timeoutHandler = () => {
+  timeout.value = timein.value;
+};
+
+const timeinHandler = () => {
+  timein.value = timeout.value;
+};
+
+timein.addEventListener('change', timeoutHandler);
+timeout.addEventListener('change', timeinHandler);
+
+const roomNumberCapacityHandler = (evt) => {
+  capacity.innerHTML = capacityOptions[evt.target.value];
+};
+
+roomNumber.addEventListener('change', roomNumberCapacityHandler);
+
+const synchronizePriceWithTypeHandler = (evt) => {
+  price.setAttribute('min', minPrices[evt.target.value]);
+  price.setAttribute('placeholder', minPrices[evt.target.value]);
+};
+
+type.addEventListener('change', synchronizePriceWithTypeHandler);
